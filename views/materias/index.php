419249107164dc32f94e62e7fe9f483a30387e18
DEUDOR <saul.21.nojodas@gmail.com>
2016-02-03 23:41:30 -0400

    revisar

--- views/materias/index.php
+++ views/materias/index.php
@@ -13,7 +13,9 @@
   
 </head>
 <body>
+
 <div class="container">
+
 	<header> <!--Cabecera del sistema web -->
 		<div class="container">
 		   <div class="row">
@@ -24,7 +26,6 @@
 		   	    <h1>Sistema de Gestion De Planes Globales</h1>	
 		   	  </div>
 		   </div>
-			
 		</div>
 	</header><!--Fin de la cabecera -->
   
@@ -66,7 +67,7 @@
   </nav><!--fin del menu horizontal-->
 	
   <section class="main row"> <!--seccion donde esta el contenido del sistema web -->
-	<br>
+  <br>
 
   <!-- Menu de navegacion vertical del sistema web-->
 	 <nav id="menu" class="col-xs-12 col-sm-4 col-md-3">
@@ -84,7 +85,7 @@
 	  </div>
 	 </nav> <!--fin del menu vertical -->
 
-     <article class="col-xs-12 col-sm-8 col-md-9"> <!--campo para visualizar el contenido del sistema web -->
+   <div class="col-xs-12 col-sm-8 col-md-9"> <!--campo para visualizar el contenido del sistema web -->
 	    <div class="container-fluid">
           <div class="col-xs-12 col-sm-8 col-md-9"> <!-- titulo del contenido que se visualiza-->
             <h2><center>Lista de Materias</center></h2>  
@@ -130,15 +131,15 @@
     	
     	
     	<div class="container-fluid"> <!-- DIV para los botones para las acciones correspondientes-->
-            <div class="row">
+          <div class="row">
              
               <!--Boton para registrar una materia -->   
-        	  <div class="col-xs-4">   
-                 <a href="<?php echo BASE_URL; ?>materias/nuevo" role="button" class="btn btn-primary">
-                   <strong>Agregar Materia</strong>
-                 </a>
-            </div>
-             
+          	  <div class="col-xs-4">   
+                   <a href="<?php echo BASE_URL; ?>materias/nuevo" role="button" class="btn btn-primary">
+                     <strong>Agregar Materia</strong>
+                   </a>
+              </div>
+               
               <!--Boton para editar una materia -->
               <div class="col-xs-4">
                 <a role="button" class="btn btn-primary" id="btnEditar" data-baseurl="<?php echo BASE_URL; ?>materias/editar">
@@ -158,55 +159,61 @@
                      <div class="modal-content"> <!--div para el contenido de la ventana emergente-->
                        
                        <!--Cabecera de la ventana-->
-                       <div class="modal-header"> 
-                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><!--Boton para cerrar la ventana emergente-->
-                         <h5 class="modal-title"> Esta seguro de eliminar esa materia?</h3> <!--Titulo del formulario de registro de materia-->
+                       <div class="modal-heder">
+                         <!--Boton para cerrar la ventana emergente-->
+                         <button type="button" class="close" data-dismiss="modal" aria-hiden="true">&times;</button>
+                         <!--Titulo del formulario de registro de materia-->
+                         <h5 class="modal-title"> Esta seguro de eliminar esa materia</h5>
                        </div>
-                       
-                       <!--Contenido de la ventana-->
-                       <div class="modal-body">
-                         
-                         <div id="nuevo"> <!--formulario para regitrar los datos de una materia-->
-                          <form name="form1" method="post" action="<?php echo BASE_URL; ?>materias/eliminar">
-                              <div class="container-fluid">
-                                  <div class="row col-xs-12 col-sm-8 col-md-12">
-                                      <button class="btn btn-primary" type="button" data-dismiss="modal" aria-hidden="true">Cancelar</button>                
-                                      <button class="btn btn-primary" type="button" id="confirmarEliminar" data-baseurl="<?php echo BASE_URL; ?>materias/eliminar">Eliminar</button>                
-                                  </div>
-                              </div>
-                           </form>
-                          </div> <!--fin del formulario para materia-->
 
-                       </div> 
+                         <!--Contenido de la ventana-->
+                         <div class="modal-body">
+                           <div id="nuevo"> <!--formulario para regitrar los datos de una materia-->
+                            <form name="form1" method="post" action="<?php echo BASE_URL; ?>materias/eliminar">
+                                <div class="container-fluid">
+                                    <div class="row col-xs-12 col-sm-8 col-md-12">
+                                        <button class="btn btn-primary" type="button" data-dismiss="modal" aria-hidden="true">Cancelar</button>                
+                                        <button class="btn btn-primary" type="button" id="confirmarEliminar" data-baseurl="<?php echo BASE_URL; ?>materias/eliminar">Eliminar</button>                
+                                    </div>
+                                </div>
+                             </form>
+                            </div> 
+                         </div> 
 
-                       <!--footer de la ventana-->
-                       <div class="modal-footer">
-                         
-                       </div>
+                          <!--footer de la ventana-->
+                          <div class="modal-footer">  </div>
+                        
+                       </div>                      
                      </div>
                    </div>
+                    <!--Fin del mensaje para eliminar-->
+
                  </div>
-              </div> 
-            </div>
-    	</div><!-- fin del DIV de los botones-->
-     </article>  
+                 <!--Fin de la funcion de eliminar-->
 
-	</section> <!--fin de la seccion donde esta el contenido -->
+             </div> 
+          </div>
+          <!-- fin del DIV de los botones-->
+
+    	</div>
+      <!--Fin del div para el contenido--> 
+
+  </section> <!--fin de la seccion donde esta el contenido -->
   
-  <footer>
-    <!--Codigo para incluir el pie de pagina-->
-    <?php 
-      include ROOT .'views'.DS.'include'.DS.'pie_de_pagina.php'
-    ?>  
-  </footer>
-
-  <!--importantando las librerias para trabajar con jquery y javascript de bootstrap-->
-	<script type="text/javascript" src="<?php echo BASE_URL; ?>public/js/jquery-1.12.0.js"></script>
-  <script type="text/javascript" src="<?php echo BASE_URL; ?>public/js/bootstrap.js"></script>
-  <script type="text/javascript" src="<?php echo BASE_URL; ?>views/materias/js/materias.js"></script>
+    <footer>
+      <!--Codigo para incluir el pie de pagina-->
+      <?php
+        include ROOT .'views'.DS.'include'.DS.'pie_de_pagina.php'
+      ?>  
+    </footer>
 
 </div>  
 
-</body>
+  <!--importantando las librerias para trabajar con jquery y javascript de bootstrap-->
+  <script type="text/javascript" src="<?php echo BASE_URL; ?>public/js/jquery-1.12.0.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_URL; ?>public/js/bootstrap.js"></script>
+  <script type="text/javascript" src="<?php echo BASE_URL; ?>views/materias/js/materias.js"></script>
+
+ </body>
 
 </html>
