d7e30e3eb2fd4c5794c605723a59e5adf6cab5e7
karloz38 <karloz4848@gmail.com>
2016-02-05 21:52:45 -0400

    añadir nueva bibliografia

--- views/planGlobal/js/crear_input.js
+++ views/planGlobal/js/crear_input.js
@@ -1,7 +1,5 @@
-//var counter = 1;
-//var limit = 5;
-//var contador = 2;
-//var txtTitulo = document.getElementById('Titulo');
+var contB=2;
+
 var contOG = 2;
 var contOE = 2;
 var contC = 2;
@@ -11,21 +9,7 @@ var numSub = 1;
 
 function addInput(divname)
 {
-    //if (counter == limit)
-    //{
-    //    alert("Llegaste al limite de Objetivos " + counter + " inputs");
-    //}
-    //else
-    //{
-    //    //var newlabel= document.createElement('label');
-    //    var newdiv = document.createElement('div');
-    //    //newlabel.innerHTML="<label for="titulo">Titulo:</label>";
-    //
-    //    counter++;
-    //contador++;
-    //}
-
-
+    
     var elemento = document.createElement("div");
 
     if(divname == 'obj_general'){
@@ -86,8 +70,36 @@ function addInput3(divname){
 
     var capitulo01 = '<div><legend>Capitulo '+(contC++)+'</legend><div class="form-group"><label for="nombre">Titulo del capitulo:</label><input class="form-control" type="text" id="tituloCapitulo" required></div>';
     var capitulo02 = '<div class="form-group col-md-6 col-md-offset-3" id="aSubtitulo'+(contSub)+'"><label for="codigo">Subtitulo:</label><input class="form-control" type="text" id="subtitulo" required></div>';
-    var capitulo03 = '<input class="btn btn-primary col-md-6 col-sm-6 col-xs-6 col-md-offset-3" type="button" value="Aniadir Subtitulo" onClick="addInput2(\'new_subtitulo\','+(contSub)+');"></div><br/><br/>';
+    var capitulo03 = '<input class="btn btn-primary col-md-6 col-sm-6 col-xs-6 col-md-offset-3" type="button" value="Añadir Subtitulo" onClick="addInput2(\'new_subtitulo\','+(contSub)+');"></div><br/><br/>';
 
 
     capitulo.innerHTML = capitulo.innerHTML + capitulo01 + capitulo02 + capitulo03;
+}
+
+function addInput4(divname){
+
+  // var elemento = document.createElement("div");
+
+   if(divname == 'n_biblio'){
+        var biblio = document.getElementById("add_biblio");
+       // biblio.appendChild(elemento);
+        //var nuevoElemento03 = '<legend>Bibliografia '+(contB++)+'</legend>';
+    }
+    var nBibliografia01 = '<legend id="separador"> </legend>';
+    //var nBibliografia02 = '<div class="container-fluid">';
+    //var nBibliografia03 = '<fieldset>';
+    var nBibliografia02 = '<div class="form-group">';
+    var nBibliografia03 = '<label for="titulo">Titulo Libro</label>';
+    var nBibliografia04 = '<input class="form-control" type="text" id="titulo">';
+    var nBibliografia05 = '</div>';
+    var nBibliografia06 = '<div class="form-group">';
+    var nBibliografia07 = '<label for="autor">Autor</label>';
+    var nBibliografia08 = '<input class="form-control" type="text" id="autor">';
+    var nBibliografia09 = '</div>';
+    var nBibliografia10 = '<div id="n_biblio">  </div>';
+    //var nBibliografia13 = '</fieldset>';
+    //var nBibliografia11 = '<br>';
+
+    biblio.innerHTML = biblio.innerHTML + nBibliografia01 + nBibliografia02 + nBibliografia03 + nBibliografia04 +
+        nBibliografia05 + nBibliografia06 + nBibliografia07 + nBibliografia08 + nBibliografia09 + nBibliografia10;
 }
